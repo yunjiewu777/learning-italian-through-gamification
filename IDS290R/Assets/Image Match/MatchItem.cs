92319bc54fcdff76f9b56cae3c27e1f5c2f3f41a
abdullahhamid2 <ahamid4@emory.edu>
2022-03-02 00:49:48 -0500

    Added word to image match game
    Minigame # 2 added.

---
+++ IDS290R/Assets/Image Match/MatchItem.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MatchItem : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerEnterHandler, IPointerUpHandler
+{
+    static MatchItem hoverItem;
+    public GameObject linePrefab;
+    public string itemName;
+    private GameObject line;
+
+    public void OnPointerDown(PointerEventData eventData){
+        line = Instantiate(linePrefab, transform.position, Quaternion.identity, transform.parent.parent.parent);
+        UpdateLine(eventData.position);
+    }
+
+    public void OnDrag(PointerEventData eventData){
+        UpdateLine(eventData.position);
+    }
+
+    public void OnPointerUp(PointerEventData eventData){
+        if(!this.Equals(hoverItem) && itemName.Equals(hoverItem.itemName)){
+            UpdateLine(hoverItem.transform.position);
+            MatchLogic.AddPoint();
+            Destroy(hoverItem);
+            Destroy(this);
+        }
+        else{
+            Destroy(line);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData){
+        hoverItem = this;
+    }
+
+    void UpdateLine(Vector3 position){
+        //update direction
+        Vector3 direction = position - transform.position;
+        line.transform.right = direction;
+
+        //update scale
+        line.transform.localScale = new Vector3(direction.magnitude, 1, 1);
+    }
+
+}
---
+++ IDS290R/Assets/Image Match/MatchLogic.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatchLogic : MonoBehaviour
+{
+    static MatchLogic Instance;
+
+    public int maxPoints = 3;
+    public Text pointsText;
+    public GameObject levelCompleteUI;
+    private int points = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;   
+    }
+
+    // Update is called once per frame
+    void UpdatePointsText()
+    {
+        pointsText.text = points + "/" + maxPoints;
+        if(points == maxPoints){
+            levelCompleteUI.SetActive(true);
+        }
+    }
+
+    public static void AddPoint(){
+        AddPoints(1);
+    }
+
+    public static void AddPoints(int points){
+        Instance.points += points;
+        Instance.UpdatePointsText();
+    }
+    //
+}
